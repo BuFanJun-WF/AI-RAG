d1dabe12d1e419eb95ceffa343baaa67acec71a8
wangfan_a <wangfan_a@aspirecn.com>
2026-03-15 23:33:53 +0800

    feat(chat): 添加聊天历史记录功能支持
    - 新增 file_history_store.py 文件实现基于文件的消息历史存储
    - 创建 FileChatMessageHistory 类用于管理会话消息历史
    - 集成 RunnableWithMessageHistory 实现对话历史记录功能
    - 添加 get_history 函数用于获取会话历史实例
    - 修改 rag.py 支持消息占位符和历史记录格式化
    - 更新调用方式支持 session_id 配置参数

---
+++ RAG项目/file_history_store.py
@@ -0,0 +1,56 @@
+import os, json
+from collections.abc import Sequence
+
+from langchain_core.messages import message_to_dict, messages_from_dict, BaseMessage
+from  langchain_core.chat_history import BaseChatMessageHistory
+
+
+# message_to_dict 单个消息对象（BaseMessage类实例）-> 字典
+# message_from_dict [字典、字典] —> [BaseMessage类实例、BaseMessage类实例]
+# AIMessage、HumanMessage、SystemMessage都是BaseMessage的子类
+
+class FileChatMessageHistory(BaseChatMessageHistory):
+    """Chat message history stored in a file."""
+    def __init__(self, session_id, storage_path):
+        self.session_id = session_id # 会话id
+        self.storage_path = storage_path # 不同会话id的存储路径
+
+        # 完整存储路径
+        self.file_path = os.path.join(self.storage_path, self.session_id + ".json")
+
+        # 确保文件夹是存在的
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+
+    def add_message(self, messages: Sequence[BaseMessage]) -> None:
+        # Sequence序列 类似list、tuple
+        all_messages = list(self.messages) # 已有的消息列表
+        all_messages.append(messages) # 新的和已有的融合成一个list
+
+        # 将数据同步写入到本地文件中
+        # 将类对象写入文件，可以将BaseMessage消息转化成字典，以json字符串的形式写入文件
+        new_message= []
+        for message in all_messages:
+            new_message.append(message_to_dict(message))
+        # 将数据写入文件
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump(new_message, f, ensure_ascii=False)
+
+    # 获取已经存在的消息
+    @property # @property装饰器将messages方法变成成员属性
+    def messages(self) -> list[BaseMessage]:
+        # 当前文件内：list[字典]
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                messages = json.load(f) # 返回值就是list[字典]
+                return messages_from_dict(messages)
+        except FileNotFoundError:
+            return []
+
+    def clear(self) -> None:
+        # 删除文件
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump([], f)
+
+# 调用这个函数获取到存储的会话实例
+def get_history(session_id):
+    return FileChatMessageHistory(session_id, "chat_history")
